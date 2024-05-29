17d6921cc2789ad274526105075eaa7320ebb563
HieuNMHE171532 <hieunmhe171532@fpt.edu.vn>
2024-05-30 01:31:43 +0700

    a

--- SWP291_Team2_SE1829NJ/src/java/dao/UserDAO.java
+++ SWP291_Team2_SE1829NJ/src/java/dao/UserDAO.java
@@ -14,7 +14,6 @@ import java.util.logging.Logger;
 import model.Account;
 import model.User;
 
-
 /**
  *
  * @author hieum
@@ -26,32 +25,62 @@ import java.sql.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class UserDAO{
+public class UserDAO {
 
     DBContext dbContext;
 
-    public UserDAO(){
+    public UserDAO() {
         dbContext = DBContext.getInstance();
     }
 
     public void createUser(User user) {
-        String name,gender,address;
+        String name, gender, address;
         Date dob;
         String sql = "INSERT INTO [User] (username, name, dob, gender, address) VALUES (?, ?, ?, ?, ?)";
-        try (Connection conn = dbContext.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-           
+        try (Connection conn = dbContext.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
             pstmt.setString(1, user.getUsername());
             pstmt.setString(2, user.getName());
-            pstmt.setDate(3,new Date (user.getDob().getTime()));
-            pstmt.setBoolean(4,user.isGender());
-            pstmt.setString(5,user.getAddress());
+            pstmt.setDate(3, new Date(user.getDob().getTime()));
+            pstmt.setBoolean(4, user.isGender());
+            pstmt.setString(5, user.getAddress());
             pstmt.executeUpdate();
             System.out.println("User created successfully!");
         } catch (SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+//    public User getUserbyUsername(Account acc) {
+//
+//        try {
+//            String sql = "SELECT * FROM [User] WHERE username = ?";
+//            Connection conn = dbContext.getConnection();
+//            PreparedStatement stm = conn.prepareStatement(sql);
+//            stm.setString(1, acc.getUsername()); // Assuming you have a getUsername method in the Account class
+//
+//            ResultSet rs = stm.executeQuery();
+//
+//            if (rs.next()) {
+//                // User found, create a User object with the retrieved data
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                Date dob = rs.getDate("dob");
+//                boolean gender = rs.getBoolean("gender");
+//                String address = rs.getString("address");
+//
+//                // Retrieve other user properties here
+//                User user = new User(id, name, dob, gender, address, name, createAt, updateAt, gender);
+//
+//                return user;
+//
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        return null;
+//    }
 
 //    public static void main(String[] args) {
 //        UserDAO userDAO = new UserDAO();
